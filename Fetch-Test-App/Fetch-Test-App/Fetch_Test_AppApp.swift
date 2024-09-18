b5454e18fd8d5827ca79770c0ceff2ee37ac85c2
Kevin Hartley <kevin.s.hartley@gmail.com>
2024-09-18 14:04:00 -0600

    initial proj commit

---
+++ Fetch-Test-App/Fetch-Test-App/Fetch_Test_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_Test_AppApp.swift
+//  Fetch-Test-App
+//
+//  Created by Kevin Hartley on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_Test_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
